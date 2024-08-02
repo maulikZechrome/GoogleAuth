4dd95bd6a17cf4dd68e94d0ea0980ec97641213f
Maulik <164501502+maulikZechrome@users.noreply.github.com>
2024-08-02 18:12:44 +0530

    added body-parser in server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
 import rootRoutes from "./routes/index"
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
-
 app.use(express.json())
+app.use(bodyParser.json({limit : Infinity}))
 app.use(express.urlencoded({ extended: true }))
 app.use(rootRoutes)
 app.listen(port, () => {
